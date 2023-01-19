e76b42e14f577e99546fe47c9bea9e1e1cb724f8
TukadiyaRaju <raju.tukadiya@gmail.com>
2023-01-19 10:41:03 +0530

    One on One page added

---
+++ src/data/ooo_profiles.js
@@ -0,0 +1,37 @@
+import profile_1 from "../assets/ooo-photos/profile-photo-1.png";
+import profile_2 from "../assets/ooo-photos/profile-photo-2.png";
+import profile_3 from "../assets/ooo-photos/profile-photo-3.png";
+import profile_4 from "../assets/ooo-photos/profile-photo-4.png";
+
+const ooo_profiles = [
+  {
+    id: 0,
+    img: profile_1,
+    name: "Lorem Ipsum",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    id: 1,
+    img: profile_2,
+    name: "Lorem Ipsum",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    id: 2,
+    img: profile_3,
+    name: "Lorem Ipsum",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    id: 3,
+    img: profile_4,
+    name: "Lorem Ipsum",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+];
+
+export default ooo_profiles;
