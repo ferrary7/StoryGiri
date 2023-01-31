7fdbe234dda19197dd05abc3bbaf687a527a4a43
Aryan Sharma <106926421+ferrary7@users.noreply.github.com>
2023-01-31 16:13:28 +0530

    Update EmailInput.js

--- src/components/EmailInput.js
+++ src/components/EmailInput.js
@@ -24,7 +24,7 @@
 // export default EmailInput;
 
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./EmailInput.css";
 import { useState } from "react";
 
