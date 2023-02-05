590c3273c38cf83c068a9b1aea7a9d819b2f0e78
ferrary7 <ary7sharma@gmail.com>
2023-02-05 16:58:51 +0530

    storygist changes

--- src/components/HomePageCOMPONENTS/StoryGist/StoryGist.js
+++ src/components/HomePageCOMPONENTS/StoryGist/StoryGist.js
@@ -10,8 +10,6 @@ function StoryGist() {
         <br /><br />
         Our experienced life coaches and trainers are here to help you overcome those challenges and achieve your goals through tailored content, practical training programs, life-changing retreats, and personalized 1-on-1 coaching sessions.
         <br /><br />
-        Our 1-on-1 coaching sessions are designed to provide a customized journey tailored specifically to your needs, with the guidance of certified and experienced coaches who are constantly exploring innovative and effective methods to ensure your journey is impactful and successful.
-        <br /><br />
         <span style={{lineHeight: '5rem', fontSize: '20px', fontWeight: '700'}}>It's time to take control of "Your Story and live it Your Way" with Storygiri.</span>
         </p>
     </div>
