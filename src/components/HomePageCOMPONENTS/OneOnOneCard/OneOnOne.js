0306c98c8939e42eeb1e8b990ef3c7d77238831a
Aditya Anand <imadityaanand24@gmail.com>
2023-02-05 14:44:39 +0530

    One on one illustration responsive

--- src/components/HomePageCOMPONENTS/OneOnOneCard/OneOnOne.js
+++ src/components/HomePageCOMPONENTS/OneOnOneCard/OneOnOne.js
@@ -6,8 +6,12 @@ const OneOnOne = () => {
   return (
     <div className="box">
       <div className="illustrationBox">
-        <h2>Ready to make your story a reality?</h2>
-        <h3>Book a 1-on-1 coaching session at Storygiri today!</h3>
+        <div>
+          <h2>Ready to make your story a reality?</h2>
+          <h3>Book a 1-on-1 coaching session at Storygiri today!</h3>
+        </div>
+        {/* <h2>Ready to make your story a reality?</h2>
+          <h3>Book a 1-on-1 coaching session at Storygiri today!</h3> */}
         <div className="email-input">
           <div className="emailContainer">
             <Subscribe btn="Book 1-1" ooo="true" />
