ecd47e0663c4683bb68a7a985f31e2c821c9f393
TukadiyaRaju <raju.tukadiya@gmail.com>
2023-02-06 21:58:40 +0530

    Shubham's DB, calendly and MailChimp integrated

--- src/components/Firebase/firebase-config.js
+++ src/components/Firebase/firebase-config.js
@@ -5,16 +5,26 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyA9mM5v68ArP0lXLSQ5tgiz62jVn0lq82g",
+//   authDomain: "storygiri-1.firebaseapp.com",
+//   projectId: "storygiri-1",
+//   storageBucket: "storygiri-1.appspot.com",
+//   messagingSenderId: "863742804424",
+//   appId: "1:863742804424:web:cd89a5251c225887aac0fd",
+//   measurementId: "G-XBVSFKW4H1",
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyA9mM5v68ArP0lXLSQ5tgiz62jVn0lq82g",
-  authDomain: "storygiri-1.firebaseapp.com",
-  projectId: "storygiri-1",
-  storageBucket: "storygiri-1.appspot.com",
-  messagingSenderId: "863742804424",
-  appId: "1:863742804424:web:cd89a5251c225887aac0fd",
-  measurementId: "G-XBVSFKW4H1",
+  apiKey: "AIzaSyAZ3klK8ApWfdjXDSwNNSCeJwC4AQREfXk",
+  authDomain: "storygiri-2.firebaseapp.com",
+  projectId: "storygiri-2",
+  storageBucket: "storygiri-2.appspot.com",
+  messagingSenderId: "617819952350",
+  appId: "1:617819952350:web:40c2744bddbc27a8a11e4f",
+  measurementId: "G-TSCW39CJM5",
 };
 
 // Initialize Firebase
